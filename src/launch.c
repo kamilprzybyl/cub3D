f2268142ca91d5149e05e0bfd45a5373cc4500b9
Kamil Przybyl <kamil776170@gmail.com>
2022-02-19 18:40:51 +0100

    hooks

--- src/launch.c
+++ src/launch.c
@@ -170,14 +170,13 @@ int	launch(void)
 	drawCeiling();
 	raycast_loop();
 	//timing for input and FPS counter
-	//data()->var.oldTime =  data()->var.time;
-	//data()->var.time = function_to_get_time(?)
-	//data()->var.frameTime = (data()->var.time - data()->var.oldTime) / 1000.0;
-	//printf(1.0 / data()->var.frameTime);
+	// data()->var.oldTime =  data()->var.time;
+	// data()->var.time = function_to_get_time(?)
+	data()->var.frameTime = 16 / 1000.0;
 
 	//speed modifiers
-	//data()->var.moveSpeed = data()->var.frameTime * 5.0;
-	//data()->var.rotSpeed = data()->var.frameTime * 3.0;
+	data()->var.moveSpeed = data()->var.frameTime * 5.0;
+	data()->var.rotSpeed = data()->var.frameTime * 3.0;
 	mlx_put_image_to_window(data()->mlx, data()->win, data()->img[4].ptr_img, 0, 0);
 	mlx_destroy_image(data()->mlx, data()->img[4].ptr_img);
 	return (0);
