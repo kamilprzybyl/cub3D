d5b5684f8569a9d165a89ab083c164987359f614
Kamil Przybyl <kamil776170@gmail.com>
2022-02-21 12:54:06 +0100

    init_colors

--- src/init/init_colors.c
+++ src/init/init_colors.c
@@ -6,27 +6,47 @@
 /*   By: kprzybyl <kprzybyl@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/20 17:17:09 by kprzybyl          #+#    #+#             */
-/*   Updated: 2022/02/20 17:17:28 by kprzybyl         ###   ########.fr       */
+/*   Updated: 2022/02/20 19:28:23 by kprzybyl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <cub3D.h>
 
+static int	validate_rgb(char **rgb)
+{
+	int	i;
+
+	i = 0;
+	while (rgb[i])
+	{
+		if (ft_atoi(rgb[i]) < 0 || ft_atoi(rgb[i]) > 255)
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
 int	init_colors(void)
 {
+	int		i;
 	char	**rgb;
 
-	rgb = ft_split(data()->rgb[0], ',');
-	if (!rgb)
-		return (1);
-	data()->floor = rgb_to_hex(ft_atoi(rgb[0]), \
-		ft_atoi(rgb[1]), ft_atoi(rgb[2]));
-	ft_free(rgb);
-	rgb = ft_split(data()->rgb[1], ',');
-	if (!rgb)
-		return (1);
-	data()->ceilling = rgb_to_hex(ft_atoi(rgb[0]), \
-		ft_atoi(rgb[1]), ft_atoi(rgb[2]));
-	ft_free(rgb);
+	i = 0;
+	while (data()->rgb[i])
+	{
+		rgb = ft_split(data()->rgb[i], ',');
+		if (!rgb)
+			return (1);
+		if (validate_rgb(rgb) == 1)
+			return (1);
+		if (i == 0)
+			data()->floor = rgb_to_hex(ft_atoi(rgb[0]), \
+				ft_atoi(rgb[1]), ft_atoi(rgb[2]));
+		if (i == 1)
+			data()->ceilling = rgb_to_hex(ft_atoi(rgb[0]), \
+				ft_atoi(rgb[1]), ft_atoi(rgb[2]));
+		ft_free(rgb);
+		i++;
+	}
 	return (0);
 }
